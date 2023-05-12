4c0f3e985ba2f5987632d4eeb4783e07df8d4139
Neordan <83787849+Neordan@users.noreply.github.com>
2023-05-12 23:02:46 +0200

    Ajout des commentaires sur le fichier js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Tableau de données pour les différentes catégories d'IMC
+
 const IMCData = [
   { name: "Maigreur", color: "midnightblue", range: [0, 18.5] },
   { name: "Bonne santé", color: "green", range: [18.5, 25] },
@@ -7,32 +9,32 @@ const IMCData = [
   { name: "Obésité morbide", color: "purple", range: 40 },
 ];
 
-// IMC = poids en kg / taille² en m
 
-// Récupérer le form
+// Sélection du formulaire dans le document HTML
 const form = document.querySelector('form');
 
-// empêcher le comportement par défaut lors de la soumission d'un formulaire HTML
+// Ajout d'un écouteur d'événement pour empêcher le comportement par défaut lors de la soumission du formulaire
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
+  
   calculateIMC();
 })
 
-// Récupérer les inputs
+// Sélection de tous les éléments 'input' du document HTML
 const inputs = document.querySelectorAll('input');
 
-// function qui va récupérer les valeurs des inputs
+// Fonction pour calculer l'IMC en utilisant les valeurs des inputs
 function calculateIMC() {
   const height = inputs[0].value;
   const weight = inputs[1].value;
-
-  //Vérifier si taille et hauteur soient non vides et positifs
+  
+  // Vérification de la validité des valeurs de hauteur et de poids
   if (!height || !weight || height <= 0 || weight <= 0) {
     Error();
     return;
   } else {
     //calcul des puissance et utilisation toFixed() pour un chiffre après la virgule
+    // IMC = poids en kg / taille² en m
     const IMC = (weight / Math.pow(height /100, 2)).toFixed(1);
     
     showResult(IMC);
@@ -40,23 +42,24 @@ function calculateIMC() {
 
 }
 
+// Sélection des éléments HTML pour afficher les résultats de l'IMC
 const valueIMC = document.querySelector('.imc-value');
 const infoIMC = document.querySelector('.imc-info');
 
-//fonction pour afficher les erreurs
+// Fonction pour afficher une erreur si les valeurs de hauteur et de poids sont incorrectes
 function Error() {
   valueIMC.textContent = "Error";
   infoIMC.textContent = "Remplissez correctement les champs"
 }
 
-// resultat en fonction de l'objet IMCData
+// Fonction pour afficher le résultat de l'IMC en fonction des données du tableau IMCData
 function showResult(IMC) {
 
   // Parcourt de l'objet pour voir où se situe l'imc dans les intervalles de range dans l'objet
   const rank = IMCData.find(data => {
     if(IMC >= data.range[0] && IMC <= data.range[1]) {
       return data;
-      // si range est un nombre
+      // si range est un nombre ?
     } else if(typeof data.range === "number" && IMC >= data.range) {
       return data;
     }
